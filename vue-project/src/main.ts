c545dd66c9ab819816c9ab9578f4af8d925789ff
bzchen <2428994017@qq.com>
2024-05-27 16:37:22 +0800

    second

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,11 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+app.use(router); // 启用 router
 
 app.mount('#app')
--- vue-project/src/views/查询.vue
+++ vue-project/src/views/查询.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="about">
       <h1>这里是查询界面</h1>
-      <span style="color: red">This should be red.</span>
     </div>
   </template>
   
