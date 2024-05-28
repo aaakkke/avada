ad530285139f9760ae0f7cf3780f9fd49aad6578
bzchen <2428994017@qq.com>
2024-05-28 13:26:26 +0800

    third

--- vue-project/components.d.ts
+++ vue-project/components.d.ts
@@ -7,6 +7,13 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElCol: typeof import('element-plus/es')['ElCol']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElHeader: typeof import('element-plus/es')['ElHeader']
+    ElMain: typeof import('element-plus/es')['ElMain']
+    ElRow: typeof import('element-plus/es')['ElRow']
+    ElTabPane: typeof import('element-plus/es')['ElTabPane']
+    ElTabs: typeof import('element-plus/es')['ElTabs']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,17 +1,39 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">   /* ts表示typesript*/
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const activeTab = ref('query')
+interface TabInfo {
+  props: {
+    name: string;
+  };
+}
+function handleTabClick(tab: TabInfo) {
+  router.push({ name: tab.props.name });
+}
 </script>
 
 <template>
-  <div id="app">
-      <div class="nav">
-          <RouterLink to="/查询">查询</RouterLink>
-          <RouterLink to="/策略">策略</RouterLink>
-          <RouterLink to="/交易">交易</RouterLink>
-          <RouterLink to="/设置">设置</RouterLink>
-      </div>
+  <el-container>
+    <el-header>
+      <el-tabs  v-model="activeTab" @tab-click="handleTabClick">
+        <el-tab-pane label="查询" name="查询"></el-tab-pane>
+        <el-tab-pane label="策略" name="策略"></el-tab-pane>
+        <el-tab-pane label="交易" name="交易"></el-tab-pane>
+        <el-tab-pane label="设置" name="设置"></el-tab-pane>
+      </el-tabs>
+    </el-header>
+    <el-main>
       <router-view/>
-    </div>
+    </el-main>
+  </el-container>
 </template>
+
+<style scoped>
+.el-main {
+  position: relative;
+  top: -50px;  /* 根据实际需要调整此值 */
+}
+</style>
 
